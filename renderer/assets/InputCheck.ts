2801ec4590e0083f06f2007bb7ccbb11856752d1
ShinDongJae <sdhsdj6450@gmail.com>
2023-02-28 19:10:02 +0900

    feat: 회원가입 vaildation and error message

--- renderer/assets/InputCheck.ts
+++ renderer/assets/InputCheck.ts
@@ -4,3 +4,9 @@ export const validateEmail = (email: string) => {
 
   return regex.test(email);
 };
+
+export const validateNickname = (nickname: string) => {
+  const regex = /^([a-zA-Z0-9ㄱ-ㅎ|ㅏ-ㅣ|가-힣]).{1,20}$/;
+
+  return regex.test(nickname);
+};
--- renderer/pages/signup.tsx
+++ renderer/pages/signup.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { validateEmail, validateNickname } from "../assets/InputCheck";
+import CompoOfInput from "../components/Input/compoOfInput";
 import { signupFuc, userProfileFuc } from "../services/auth";
 import { signupDB } from "../services/database";
 
@@ -15,35 +17,48 @@ const Signup = () => {
   const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (email !== "" && password !== "" && passwordConfirm !== "") {
-      if (password !== passwordConfirm) {
-        setErrorMsg("비밀번호를 확인해주세요.");
-        console.log(errorMsg);
-      }
-      if (password === passwordConfirm) {
-        try {
-          await signupFuc(email, password).then(() => {
-            userProfileFuc(nickname);
-            signupDB(email, nickname);
-            router.push("/home");
-            setEmail("");
-            setPassword("");
-            setNickname("");
-            setPasswordConfirm("");
-            setErrorMsg("");
-          });
-        } catch (error) {
-          switch (error.code) {
-            case "auth/weak-password":
-              setErrorMsg("비밀번호는 6자리 이상이어야 합니다");
-              break;
-            case "auth/invalid-email":
-              setErrorMsg("잘못된 이메일 주소입니다");
-              break;
-            case "auth/email-already-in-use":
-              setErrorMsg("이미 가입되어 있는 계정입니다");
-              break;
-          }
+    if (email === "") return setErrorMsg("이메일을 입력해주세요.");
+    if (!validateEmail(email))
+      return setErrorMsg("이메일을 올바르게 입력해주세요.");
+
+    if (nickname === "") return setErrorMsg("닉네임을 입력해주세요.");
+    if (!validateNickname(nickname))
+      return setErrorMsg(
+        "닉네임은 한글, 영문, 숫자 포함 1~20자 이상 입력해야합니다."
+      );
+
+    if (password === "") return setErrorMsg("비밀번호를 입력해주세요");
+    if (passwordConfirm === "")
+      return setErrorMsg("비밀번호를 한번 더 입력해주세요");
+
+    if (
+      email !== "" &&
+      nickname !== "" &&
+      password !== "" &&
+      passwordConfirm !== ""
+    ) {
+      if (password.length < 5 || password.length > 20)
+        return setErrorMsg("비밀번호는 5~20자 입력해야합니다.");
+
+      if (password !== passwordConfirm)
+        return setErrorMsg("비밀번호를 다시 확인해주세요.");
+
+      try {
+        await signupFuc(email, password).then(() => {
+          userProfileFuc(nickname);
+          signupDB(email, nickname);
+          router.push("/home");
+          setEmail("");
+          setPassword("");
+          setNickname("");
+          setPasswordConfirm("");
+          setErrorMsg("");
+        });
+      } catch (error) {
+        switch (error.code) {
+          case "auth/email-already-in-use":
+            setErrorMsg("이미 사용 중인 이메일입니다");
+            break;
         }
       }
     }
@@ -52,45 +67,26 @@ const Signup = () => {
     <div className="w-screen h-screen flex flex-col justify-center">
       <div className="w-96 h-72 m-auto flex flex-col justify-center ">
         <form className="pt-5 pb-5" onSubmit={onSubmitHandler}>
-          <div className="w-full flex">
-            <input
-              placeholder="이메일을 입력해주세요."
-              className="border py-2 px-2 text-grey-darkest w-full"
-              type="email"
-              onChange={e => {
-                setEmail(e.target.value);
-              }}
-            />
-          </div>
-          <div className="w-full mt-5">
-            <input
-              placeholder="닉네임을 입력해주세요."
-              className="border py-2 px-2 text-grey-darkest w-full"
-              type="text"
-              onChange={e => {
-                setNickname(e.target.value);
-              }}
-            />
-          </div>
-          <div className="w-full mt-5">
-            <input
-              placeholder="비밀번호를 입력해주세요."
-              className="border py-2 px-2 text-grey-darkest w-full"
-              type="password"
-              onChange={e => {
-                setPassword(e.target.value);
-              }}
-            />
-          </div>
-          <div className="w-full mt-5">
-            <input
-              placeholder="비밀번호를 한번 더 입력해주세요."
-              className="border py-2 px-2 text-grey-darkest w-full"
-              type="password"
-              onChange={e => {
-                setPasswordConfirm(e.target.value);
-              }}
-            />
+          <CompoOfInput
+            placeholder="이메일을 입력해주세요."
+            setChange={setEmail}
+          />
+          <CompoOfInput
+            placeholder="닉네임을 입력해주세요."
+            setChange={setNickname}
+          />
+          <CompoOfInput
+            placeholder="비밀번호를 입력해주세요."
+            type="password"
+            setChange={setPassword}
+          />
+          <CompoOfInput
+            placeholder="비밀번호를 한번 더 입력해주세요."
+            type="password"
+            setChange={setPasswordConfirm}
+          />
+          <div className="w-full mb-5">
+            <span className="text-[red]">{errorMsg}</span>
           </div>
           <div className="w-full mt-5 flex justify-around">
             <button
